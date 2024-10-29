ea93050d374d57431432d904bd1d9c110b9420ff
aaanishaaa <anishaworks21@gmail.com>
2024-10-30 02:41:23 +0530

    basic interface

---
+++ base/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Task
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy #goes back to the same page after submission
+
+class TaskList(ListView):   # Class-based view for listing tasks
+    model = Task
+    context_object_name = 'tasks'  # Define the context variable used in the template
+
+class TaskDetail(DetailView):  # Class-based view for showing task details
+    model = Task
+    context_object_name = 'task'  # Define the context variable used in the template
+    template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks') #redirects users to task
